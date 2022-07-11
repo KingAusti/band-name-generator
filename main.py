e9ea4d60f864e8d91fdb7601726dc2f9f4f9716c
KingAusti <austin@thehenrys.xyz>
2022-07-10 17:49:16 -0700

    finishing band name generator

---
+++ main.py
@@ -0,0 +1,4 @@
+print('welcome to the bandname generator');
+city = input('What city did you grow up in?\n');
+pet = input('What is the name of your pet?\n');
+print('your band name could be ' + city + ' ' + pet)
